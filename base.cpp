8f498eab6bc8a89655560d59c999df2917c979f7
Luiz Felipe Cabral Lima <luizfelipecabrallima@gmail.com>
2024-04-11 19:34:41 -0300

    Função para impressão do resultado configurada

--- base.cpp
+++ base.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <stdio.h>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <list>
+#include <unistd.h>
+#include <vector>
 
 
 
@@ -66,6 +69,34 @@ void roundRobin(list<Process> &Ps, list<planning> &plan) {
     }
 }
 
+void printing(int &tempo,vector<int> &result, int &posR) {
+    string resu("");
+    int aux = 0;
+    int proc = 0;
+    while (!(result.empty())){
+        if (result[0] != aux) {
+            resu.append("    |");
+            aux++;
+            proc++;
+        } else {
+            if (result[0] == posR) {
+                resu.append(" ## |");
+            } else {
+                resu.append(" -- |");
+            }
+            aux++;
+            result.erase(result.begin());
+            proc++;
+        }
+    }
+    if (proc < 4){
+        for(int i = 0; i < (4-proc); i++){
+            resu.append("    |");
+        }
+    }
+    printf("%02d-%02d |%s\n", tempo-1,tempo, resu.c_str());
+}
+
 void EDF(list<Process> &Ps, list<planning> &plan) {
     int tempo = 0;
     bool flag = true;
@@ -73,11 +104,13 @@ void EDF(list<Process> &Ps, list<planning> &plan) {
     int pos;
     int posR;
 
+    printf("\ntempo | P1 | P2 | P3 | P4 |\n");
     while (flag) {
         flag = false;
         timeflag = true;
         pos = 0;
         planning t;
+        vector<int> result;
         bool full = false;
         for (Process p : Ps) {
             if (p.duration > 0) {
@@ -90,12 +123,14 @@ void EDF(list<Process> &Ps, list<planning> &plan) {
                         t.deadline = p.deadline;
                         posR = pos;
                         full = true;
+                        result.push_back(posR);
                     } else if (p.deadline < t.deadline) {
                         t.pid = p.pid;
                         t.tempo = tempo;
                         t.duration = p.duration;
                         t.deadline = p.deadline;
                         posR = pos;
+                        result.push_back(posR);
                     }
                 }
             }
@@ -107,6 +142,10 @@ void EDF(list<Process> &Ps, list<planning> &plan) {
         advance(it, posR);
         (*it).duration -= 1;
         tempo += 1;
+
+        printing(tempo, result, posR);
+
+        sleep(1);
     }
 }
 
@@ -120,7 +159,7 @@ int main() {
     P1.start = 0;
     P1.duration = 5;
     P1.priority = 2;
-    P1.deadline = 10;
+    P1.deadline = 10 + P1.start;
     Ps.push_back(P1);
 
     Process P2;
@@ -128,7 +167,7 @@ int main() {
     P2.start = 0;
     P2.duration = 2;
     P2.priority = 3;
-    P2.deadline = 20;
+    P2.deadline = 20 + P2.start;
     Ps.push_back(P2);
 
     Process P3;
@@ -136,7 +175,7 @@ int main() {
     P3.start = 1;
     P3.duration = 4;
     P3.priority = 1;
-    P3.deadline = 15;
+    P3.deadline = 15 + P3.start;
     Ps.push_back(P3);
 
     Process P4;
@@ -144,7 +183,7 @@ int main() {
     P4.start = 3;
     P4.duration = 3;
     P4.priority = 4;
-    P4.deadline = 18;
+    P4.deadline = 18 + P4.start;
     Ps.push_back(P4);
 
 
@@ -155,10 +194,10 @@ int main() {
 
 
 
-    list<planning>::iterator it;
+    /*list<planning>::iterator it;
     for (it = plan.begin(); it != plan.end(); ++it) {
         cout << it->tempo << '|' << it->pid << '\t';
-    }
+    }*/
 }
 
 
