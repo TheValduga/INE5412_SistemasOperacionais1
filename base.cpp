5131b70ed83296beae20dcfb6ee8e9984ee9cda6
Gabriel-Turatti <75402522+Gabriel-Turatti@users.noreply.github.com>
2024-04-13 14:55:05 -0300

    Adiciona periodo, construct, e corrige print

--- base.cpp
+++ base.cpp
@@ -15,9 +15,29 @@ class Process {
     public:
         int pid;
         int start;
+        int end;
         int duration;
+        int durationMax;
         int priority;
-        int deadline;
+        char state; // (N)ovo, (P)ronto, (E)xecutando, (T)erminado
+        int usedtime;
+        int period;
+        int DLcur; // Deadline Current
+        int DLmax; // Deadline Max
+        int queue; // quando o processo já passou do periodo e existe outra instancia esperando execução
+        Process(int pidN, int startN, int durationN, int priorityN, int periodN) {
+            pid = pidN;
+            start = startN;
+            state = 'N';
+            duration = durationN;
+            durationMax = durationN;
+            priority = priorityN;
+            period = periodN;
+            DLcur = periodN;
+            DLmax = periodN;
+            queue = 0;
+            usedtime = 0;
+        }
 };
 
 struct scheduler_turn {
@@ -28,10 +48,9 @@ struct scheduler_turn {
 struct planning {
     int pid;
     int tempo;
-    int deadline;
+    int DLcur;
     int duration;
 };
-
 
 
 
@@ -69,7 +88,7 @@ void roundRobin(list<Process> &Ps, list<planning> &plan) {
     }
 }
 
-void printing(int &tempo,vector<int> &result, int &posR) {
+void printing(int &tempo, vector<int> &result, int &posR) {
     string resu("");
     int aux = 0;
     int proc = 0;
@@ -116,36 +135,55 @@ void EDF(list<Process> &Ps, list<planning> &plan) {
             if (p.duration > 0) {
                 flag = true;
                 if (p.start <= tempo) {
+                    p.state = 'P';
+                    result.push_back(pos);
                     if (not full) {
                         t.pid = p.pid;
                         t.tempo = tempo;
                         t.duration = p.duration;
-                        t.deadline = p.deadline;
+                        t.DLcur = p.DLcur;
                         posR = pos;
                         full = true;
-                        result.push_back(posR);
-                    } else if (p.deadline < t.deadline) {
+                    } else if (p.DLcur < t.DLcur) {
                         t.pid = p.pid;
                         t.tempo = tempo;
                         t.duration = p.duration;
-                        t.deadline = p.deadline;
+                        t.DLcur = p.DLcur;
                         posR = pos;
-                        result.push_back(posR);
                     }
                 }
+            } else if (p.queue > 0) {
+                result.push_back(pos);
+                p.duration = p.durationMax;
+                p.DLcur += p.DLmax;
+                p.queue -= 1;
+            } else {
+                p.state = 'T';
             }
+            if (tempo % p.period == 0 && tempo != 0) {
+                if (p.duration != 0) {
+                    p.queue += 1;
+                } else {
+                    p.duration = p.durationMax;
+                }
+            }
+            list<Process>::iterator it2 = Ps.begin();
+            advance(it2, pos);
+            (*it2) = p;
             pos += 1;
         }
+
 
         plan.push_back(t);
         list<Process>::iterator it = Ps.begin();
         advance(it, posR);
         (*it).duration -= 1;
+        (*it).state = 'E';
         tempo += 1;
 
         printing(tempo, result, posR);
 
-        sleep(1);
+        sleep(0.1);
     }
 }
 
@@ -154,38 +192,20 @@ void EDF(list<Process> &Ps, list<planning> &plan) {
 int main() {
     list<Process> Ps;
 
-    Process P1;
-    P1.pid = 0;
-    P1.start = 0;
-    P1.duration = 5;
-    P1.priority = 2;
-    P1.deadline = 10 + P1.start;
+    Process P1(0, 0, 5, 1, 20);
     Ps.push_back(P1);
 
-    Process P2;
-    P2.pid = 1;
-    P2.start = 0;
-    P2.duration = 2;
-    P2.priority = 3;
-    P2.deadline = 20 + P2.start;
+    Process P2(1, 0, 2, 4, 8);
     Ps.push_back(P2);
 
-    Process P3;
-    P3.pid = 2;
-    P3.start = 1;
-    P3.duration = 4;
-    P3.priority = 1;
-    P3.deadline = 15 + P3.start;
+    Process P3(2, 1, 4, 2, 16);
     Ps.push_back(P3);
 
-    Process P4;
-    P4.pid = 3;
-    P4.start = 3;
-    P4.duration = 3;
-    P4.priority = 4;
-    P4.deadline = 18 + P4.start;
+    Process P4(3, 3, 3, 3, 12);
     Ps.push_back(P4);
 
+
+    printf("%d", P1.duration);
 
     list<planning> plan;
     // roundRobin(Ps, plan);
